f266d58ec2cdd558ea2558db79fa844e76c765b7
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-02-20 18:04:38 -0500

    Done in class

--- ICE6_starter/scripts/fetch_script.js
+++ ICE6_starter/scripts/fetch_script.js
@@ -1,16 +1,34 @@
 console.log("fetch_script.js loaded");
 
 // get the button for fetch
+let btn_fetch = $('#fetchJoke');
 
 // create a url variable
+let url_fetch = "https://icanhazdadjoke.com/";
 
 //need a header Accept value set to 'application/json'
+let myFetchHeader = {"Accept": "application/json"};
 
 // create the callback for the click
+$(btn_fetch).click(() => 
+{
     // use fetch
-        // give the necessary header data
+    fetch(url_fetch, 
+        {
+            // give the necessary header data
+            headers: myFetchHeader
+        })
         // first then() to recieve the promise
+        .then((result) => 
+        {
             // send the json from the promise on to the next then()
+            return result.json();
+        }).then((jsonRes) => 
+        {
             // console log the json
-            // set the output
+            console.log(jsonRes);
+            // set the output // output id="output"
+        })
+});
+
 
--- ICE6_starter/scripts/xhr_script.js
+++ ICE6_starter/scripts/xhr_script.js
@@ -1,19 +1,43 @@
 console.log("xhr_script.js loaded");
 
 // get the button for xhr
+let btn_xhr = $('#xhrJoke');
 
 // create a url variable
+let url_xhr = "https://icanhazdadjoke.com/";
 
 // create a click callback that handles the API call
+$(btn_xhr).click(() => 
+{
     // instantiate an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+
     // confiture with open()
+    xhr.open("GET", url_xhr)
+
     // set the necessary headers for icanhazdadjoke.com
+    xhr.setRequestHeader("Accept", "application/json");
+
     // create the callback to handle the onreadystatechange
+    xhr.onreadystatechange = function () 
+    {
         // only execute when done and status is 200
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200)
+        {
             // get the responseText
+            let joke = xhr.responseText;
+
             // console log
+            console.log(joke);
+
             // JSON.parse the joke
-            // set the output
+            // set the output // output id="output"
+        }
+    }
     // send the request
+    xhr.send();
+
+});
+
 
 
